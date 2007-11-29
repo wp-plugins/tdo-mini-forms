dffcc61b886e1d6127ba6c0d74f7e590af6eff50
the_dead_one <the_dead_one@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-11-29 17:41:39 +0000

    small update to the tags widget
    git-svn-id: https://plugins.svn.wordpress.org/tdo-mini-forms/trunk@25676 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/tdomf-tags-widget.php
+++ widgets/tdomf-tags-widget.php
@@ -13,74 +13,83 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('TDOM
 #TODO: Allow user to specify title, size and overwrite default tags
 #TODO: Add option to core for default tags
 
-//////////////////////////////
-// Add text field to display tags.
+// Only enable this widget if tags are avaliable in the installation
 //
-function tdomf_widget_tags($args) {
-  extract($args);
-  $output  = $before_widget;  
-  $output .= '<label for="tags" >';
-  $output .= __("Tags (separate multiple tags with commas: cats, pet food, dogs):","tdomf");
-  $output .= '<br/><input type="textfield" id="tags" name="tags" size="60" value="'.$tags.'" />';
-  $output .= '</label>';
-  $output .= $after_widget;
-  return $output;
-  }
-tdomf_register_form_widget('tags','Tags', 'tdomf_widget_tags');
-
-////////////////////////
-// Preview tags
+// TDOMF is only meant to support WP2.3... but ppl still ask for 2.2 support
+// so this function call will disable this widget in 2.3
 //
-function tdomf_widget_tags_preview($args) {
-  extract($args);
+if(function_exists('wp_set_post_tags')) {
 
-  if(isset($tags) && !empty($tags)) {
-    $output  = $before_widget;
-    $output .= sprintf(__("<b>Post will be sumbmitted with these tags</b>:<br/>%s","tdomf"), strip_tags($tags));  
+  //////////////////////////////
+  // Add text field to display tags.
+  //
+  function tdomf_widget_tags($args) {
+    extract($args);
+    $output  = $before_widget;  
+    $output .= '<label for="tags" >';
+    $output .= __("Tags (separate multiple tags with commas: cats, pet food, dogs):","tdomf");
+    $output .= '<br/><input type="textfield" id="tags" name="tags" size="60" value="'.$tags.'" />';
+    $output .= '</label>';
     $output .= $after_widget;
     return $output;
-  }
-  
-  return "";
-}
-tdomf_register_form_widget_preview('tags','Tags', 'tdomf_widget_tags_preview');
-
-
-////////////////////////
-// Add tags to the post
-//
-function tdomf_widget_tags_post($args) {
-  extract($args);
-
-  if(isset($tags) && !empty($tags)) {
-     # set last var to true to just append
-     wp_set_post_tags($post_ID, strip_tags($tags),false);
-  }
- 
-  return NULL;
-}
-tdomf_register_form_widget_post('tags','Tags', 'tdomf_widget_tags_post');
-
-///////////////////////////////////////////////////////////
-// Show what tags are on the post to admins for moderating
-//
-function tdomf_widget_tags_adminemail($args) {
-  extract($args);
-
-  $tags = wp_get_post_tags($post_ID);
-  
-  if(!empty($tags)) {
-    $output  = $before_widget;
-    $output .= __("Post tagged with\r\n","tdomf");
-    foreach($tags as $tag) {
-      $output .= $tag->name.", ";
     }
-    $output .= $after_widget;
-    return $output;
-  }
+  tdomf_register_form_widget('tags','Tags', 'tdomf_widget_tags');
   
-  return "";
+  ////////////////////////
+  // Preview tags
+  //
+  function tdomf_widget_tags_preview($args) {
+    extract($args);
+  
+    if(isset($tags) && !empty($tags)) {
+      $output  = $before_widget;
+      $output .= sprintf(__("<b>Post will be sumbmitted with these tags</b>:<br/>%s","tdomf"), strip_tags($tags));  
+      $output .= $after_widget;
+      return $output;
+    }
+    
+    return "";
+  }
+  tdomf_register_form_widget_preview('tags','Tags', 'tdomf_widget_tags_preview');
+  
+  
+  ////////////////////////
+  // Add tags to the post
+  //
+  function tdomf_widget_tags_post($args) {
+    extract($args);
+  
+    if(isset($tags) && !empty($tags)) {
+       # set last var to true to just append
+       wp_set_post_tags($post_ID, strip_tags($tags),false);
+    }
+   
+    return NULL;
+  }
+  tdomf_register_form_widget_post('tags','Tags', 'tdomf_widget_tags_post');
+  
+  ///////////////////////////////////////////////////////////
+  // Show what tags are on the post to admins for moderating
+  //
+  function tdomf_widget_tags_adminemail($args) {
+    extract($args);
+  
+    $tags = wp_get_post_tags($post_ID);
+    
+    if(!empty($tags)) {
+      $output  = $before_widget;
+      $output .= __("Post tagged with\r\n","tdomf");
+      foreach($tags as $tag) {
+        $output .= $tag->name.", ";
+      }
+      $output .= $after_widget;
+      return $output;
+    }
+    
+    return "";
+  }
+  tdomf_register_form_widget_adminemail('tags','Tags', 'tdomf_widget_tags_adminemail');
+
 }
-tdomf_register_form_widget_adminemail('tags','Tags', 'tdomf_widget_tags_adminemail');
 
 ?>
