8bad3bafc0011f72b82c3815bdc2416312753a54
the_dead_one <the_dead_one@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-20 14:25:03 +0000

    reCapatcha widget for editing forms complete!
    git-svn-id: https://plugins.svn.wordpress.org/tdo-mini-forms/trunk@119194 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/tdomf-recaptcha-widget.php
+++ widgets/tdomf-recaptcha-widget.php
@@ -3,7 +3,7 @@
 Name: "reCaptcha"
 URI: http://thedeadone.net/software/tdo-mini-forms-wordpress-plugin/
 Description: Use recaptcha to verify user input
-Version: 2
+Version: 3
 Author: Mark Cunningham
 Author URI: http://thedeadone.net
 */
@@ -13,161 +13,191 @@ if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('TDOM
 // Path to recaptchalib.php
 define("TDOMF_RECAPTCHALIB_PATH",TDOMF_WIDGET_PATH.'recaptcha/recaptchalib.php');
 
+  /** 
+   * reCaptcha Widget. Use recaptcha to verify user input
+   * 
+   * @author Mark Cunningham <tdomf@thedeadone.net> 
+   * @version 3.0 
+   * @since 0.13.0
+   * @access public 
+   * @copyright Mark Cunningham
+   * 
+   */ 
+  class TDOMF_WidgetReCaptcha extends TDOMF_Widget
+  {
+      /** 
+       * Initilise and start widget
+       * 
+       * @access public
+       */ 
+      function TDOMF_WidgetReCaptcha() {
+          $this->enableValidate();
+          $this->enableWidgetTitle();
+          $this->enableControl(true,700,500);
+          $this->setInternalName('recaptcha');
+          $this->setDisplayName(__('reCaptcha','tdomf'));
+          $this->setOptionKey('tdomf_recaptcha_widget');
+          $this->setModes(array('new','edit'));
+          $this->enableAdminError();
+          $this->start();
+      }
+
 /* 1. report error */
+      
+      /**
+       * What to display in form
+       * 
+       * @access public
+       * @return String
+       */
+      function form($args,$options) {
+         extract($args);
+         $output = "";
+         
+         if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") {
+             $use_ssl = true;
+         } else {
+             $use_ssl = false;
+         }
 
-  function tdomf_widget_recaptcha($args) {
-    $options = tdomf_widget_recaptcha_get_options($args['tdomf_form_id']);
+         if(!function_exists('recaptcha_get_html')) {
+             @require_once(TDOMF_RECAPTCHALIB_PATH);
+         }
     
-    extract($args);
-    
-    $output = $before_widget;
-
-    if(!empty($options['title'])) {
-        $output .= $before_title;
-        $output .= $options['title'];
-        $output .= $after_title;
-    }
-    
-    if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") {
-        $use_ssl = true;
-    } else {
-        $use_ssl = false;
-    }
-
-    if(!function_exists('recaptcha_get_html')) {
-        @require_once(TDOMF_RECAPTCHALIB_PATH);
-    }
-    
-    $output .= <<<END
+         $output .= <<<END
 		<script type='text/javascript'>
 		var RecaptchaOptions = { theme : '{$options['theme']}', lang : '{$options['language']}' , tabindex : 30 };
 		</script>
 END;
 
-    $form_data = tdomf_get_form_data($args['tdomf_form_id']);
-    $error = null;
-    if(isset($form_data['recaptcha_error'])) {
-        $error = $form_data['recaptcha_error'];
-    }
+        $form_data = tdomf_get_form_data($args['tdomf_form_id']);
+        $error = null;
+        if(isset($form_data['recaptcha_error'])) {
+            $error = $form_data['recaptcha_error'];
+        }
 
-    $output .= recaptcha_get_html ($options['publickey'], $error, $use_ssl, $options['xhtml']);
-    
-    $output .= $after_widget;
-    
-    return $output;
-  }
-  tdomf_register_form_widget('recaptcha',__('reCaptcha',"tdomf"), 'tdomf_widget_recaptcha');
- 
- 
-function tdomf_widget_recaptcha_validate($args,$preview) {
-    
-  // don't bother validating for preview
-  if($preview) {
-    return NULL;
-  }
+        $output .= recaptcha_get_html ($options['publickey'], $error, $use_ssl, $options['xhtml']);
+        return $output;
+      }
+      
+      /**
+       * Validate widget input
+       * 
+       * @access public
+       * @return Mixed
+       */
+      function validate($args,$options,$preview) {
+          
+           // don't bother validating for preview
+           if($preview) {
+               return NULL;
+           }
+          
+          extract($args);
+          $output = "";
+          
+          if (empty($args['recaptcha_response_field'])) {
+              return __('Please complete the reCAPTCHA.','tdomf');
+          }
+            
+          if(!function_exists('recaptcha_check_answer')) {
+              @require_once(TDOMF_RECAPTCHALIB_PATH);
+          }  
   
-  $options = tdomf_widget_recaptcha_get_options($args['tdomf_form_id']);
-  
-  tdomf_log_message('dump<pre>' . var_export($args,true) . '</pre>');
-  
-  if (empty($args['recaptcha_response_field'])) {
-      return __('Please complete the reCAPTCHA.','tdomf');
-  }
+          $response = recaptcha_check_answer($options['privatekey'],
+              $_SERVER['REMOTE_ADDR'],
+              $args['recaptcha_challenge_field'],
+              $args['recaptcha_response_field']);
 
-  if(!function_exists('recaptcha_check_answer')) {
-      @require_once(TDOMF_RECAPTCHALIB_PATH);
-  }  
-  
-    $response = recaptcha_check_answer($options['privatekey'],
-        $_SERVER['REMOTE_ADDR'],
-        $args['recaptcha_challenge_field'],
-        $args['recaptcha_response_field']);
-
-    if (!$response->is_valid) {
-        $form_data = tdomf_get_form_data($args['tdomf_form_id']);  
-        $form_data['recaptcha_error'] = $response->error;
-        tdomf_save_form_data($args['tdomf_form_id'],$form_data);
-        if ($response->error == 'incorrect-captcha-sol') {
+          if (!$response->is_valid) {
+              $form_data = tdomf_get_form_data($args['tdomf_form_id']);  
+              $form_data['recaptcha_error'] = $response->error;
+              tdomf_save_form_data($args['tdomf_form_id'],$form_data);
+              if ($response->error == 'incorrect-captcha-sol') {
                 return __('That reCAPTCHA was incorrect.','tdomf');
-        } else {
+              } else {
                 tdomf_log_message('reCAPTCHA error ' . $response->error . '. Please refer to <a href="http://recaptcha.net/apidocs/captcha/">reCaptcha docs</a> for more information', TDOMF_LOG_ERROR);
                 return __('Invalid reCAPTCHA configuration.','tdomf');
-        }
-    }
-  return NULL;
-}
-tdomf_register_form_widget_validate('recaptcha',__('reCaptcha',"tdomf"), 'tdomf_widget_recaptcha_validate', array());
-  
-  
-  function tdomf_widget_recaptcha_get_options($form_id,$strict=false) {
-        $options = tdomf_get_option_widget('tdomf_recaptcha_widget',$form_id);
-        $recaptcha_options = get_option('recaptcha');
-        if($options == false) {
-           $options = array();
-           $options['title'] = "";
-           $options['publickey'] = "";
-           $options['privatekey'] = "";
-           $options['theme'] = 'red'; 
-           $options['language'] = 'en';
-           $options['xhtml'] = false;
-           $options['plugin'] = false;
-           if($recaptcha_options != false) {
-                   $options['publickey'] = $recaptcha_options['pubkey'];
-                   $options['privatekey'] = $recaptcha_options['privkey'];
-                   $options['theme'] = $recaptcha_options['re_theme']; 
-                   $options['language'] = $recaptcha_options['re_lang'];
-                   $options['xhtml'] = $recaptcha_options['re_xhtml'];
-           }
-        } else if(!$strict && $options['plugin']) {
-           if($recaptcha_options != false) {
-                   $options['publickey'] = $recaptcha_options['pubkey'];
-                   $options['privatekey'] = $recaptcha_options['privkey'];
-                   $options['theme'] = $recaptcha_options['re_theme']; 
-                   $options['language'] = $recaptcha_options['re_lang'];
-                   $options['xhtml'] = $recaptcha_options['re_xhtml'];
-           }
-        }
-      return $options;
-    }
-  
-  function tdomf_widget_recaptcha_control($form_id) {
-      $options = tdomf_widget_recaptcha_get_options($form_id,true);
-  
-  // Store settings for this widget
-    if ( $_POST['excerpt-submit'] ) {
-           $newoptions['title'] = $_POST['recaptcha-title'];
-           $newoptions['publickey'] = $_POST['recaptcha-publickey'];
-           $newoptions['privatekey'] = $_POST['recaptcha-privatekey'];
-           $newoptions['theme'] = $_POST['recaptcha-themekey']; 
-           $newoptions['language'] = $_POST['recaptcha-language'];
-           $newoptions['xhtml'] = isset($_POST['recaptcha-xhtml']);
-           $newoptions['plugin'] = isset($_POST['recaptcha-plugin']);
-     if ( $options != $newoptions ) {
-        $options = $newoptions;
-        tdomf_set_option_widget('tdomf_recaptcha_widget', $options,$form_id);
-     }
-  }
+              }
+          }
+          return NULL;
+      }
+      
+      /**
+       * Overrides "getOptions" with defaults for this widget
+       * 
+       * @access public
+       * @return String
+       */
+      function getOptions($form_id) {
+          $defaults = array(   'publickey' => '',
+                               'privatekey' => '',
+                               'theme' => 'red',
+                               'language' => 'en',
+                               'xhtml' => false,
+                               'plugin' => false,
+                               'text-rows' => 10, 
+                               'quicktags' => false,
+                               'restrict-tags' => true,
+                               'allowable-tags' => "<p><b><em><u><strong><a><img><table><tr><td><blockquote><ul><ol><li><br><sup>",
+                               'char-limit' => 0,
+                               'word-limit' => 0 );
+          $recaptcha_options = get_option('recaptcha');
+          if($recaptcha_options != false) {
+              $defaults['publickey'] = $recaptcha_options['pubkey'];
+              $defaults['privatekey'] = $recaptcha_options['privkey'];
+              $defaults['theme'] = $recaptcha_options['re_theme']; 
+              $defaults['language'] = $recaptcha_options['re_lang'];
+              $defaults['xhtml'] = $recaptcha_options['re_xhtml'];
+          }
+          $options = TDOMF_Widget::getOptions($form_id); 
+          $options = wp_parse_args($options, $defaults);
+          if($options['plugin'] && $recaptcha_options != false) {
+              $options['publickey'] = $recaptcha_options['pubkey'];
+              $options['privatekey'] = $recaptcha_options['privkey'];
+              $options['theme'] = $recaptcha_options['re_theme']; 
+              $options['language'] = $recaptcha_options['re_lang'];
+              $options['xhtml'] = $recaptcha_options['re_xhtml'];
+          }
+          return $options;
+      }   
+      
+      /**
+       * Configuration panel for widget
+       * 
+       * @access public
+       */      
+      function control($options,$form_id) {
+          
+          // Store settings for this widget
+          //
+          if ( $_POST[$this->internalName.'-submit'] ) {
+                 $newoptions['publickey'] = $_POST['recaptcha-publickey'];
+                 $newoptions['privatekey'] = $_POST['recaptcha-privatekey'];
+                 $newoptions['theme'] = $_POST['recaptcha-themekey']; 
+                 $newoptions['language'] = $_POST['recaptcha-language'];
+                 $newoptions['xhtml'] = isset($_POST['recaptcha-xhtml']);
+                 $newoptions['plugin'] = isset($_POST['recaptcha-plugin']);
+                 $options = wp_parse_args($newoptions, $options);
+                 $this->updateOptions($options,$form_id);
+          }
 
-    if(!function_exists('recaptcha_get_signup_url')) {
-      @require_once(TDOMF_RECAPTCHALIB_PATH);
-    }  
-  
-    // get blog domain
-    $uri = parse_url(get_settings('siteurl'));
-	$blogdomain = $uri['host'];
-    
-   // Display control panel for this widget
-  
-  extract($options);
-
-        ?>
-<div>
-    <label for="recaptcha-title" style="line-height:35px;"><?php _e("Title: ","tdomf"); ?></label>
-    <input type="textfield" id="recaptcha-title" name="recaptcha-title" value="<?php echo htmlentities($options['title'],ENT_QUOTES,get_bloginfo('charset')); ?>" />
-
-    <br/><br/>
-
-    <small><?php _e('If this option is enabled and the reCaptcha plugin is active, the settings for comments will overwrite any configuration below.','tdomf'); ?></small><br/>
+          if(!function_exists('recaptcha_get_signup_url')) {
+              @require_once(TDOMF_RECAPTCHALIB_PATH);
+          }  
+          
+          // get blog domain
+          $uri = parse_url(get_settings('siteurl'));
+          $blogdomain = $uri['host'];
+          
+          // Display control panel for this widget
+          //
+          extract($options);
+          ?>
+<div>          
+          <?php $this->controlCommon($options); ?>
+          
+          <small><?php _e('If this option is enabled and the reCaptcha plugin is active, the settings for comments will overwrite any configuration below.','tdomf'); ?></small><br/>
     <input type="checkbox" name="recaptcha-plugin" id="recaptcha-plugin" <?php if($options['plugin']) echo "checked"; ?> >
     <label for="recaptcha-plugin" style="line-height:35px;"><?php _e('Use comment settings from <a href="http://wordpress.org/extend/plugins/wp-recaptcha/">reCaptcha plugin</a> if active',"tdomf"); ?></label>
     
@@ -221,20 +251,28 @@ tdomf_register_form_widget_validate('recaptcha',__('reCaptcha',"tdomf"), 'tdomf_
     </div>
     
 <?php
-}
-tdomf_register_form_widget_control('recaptcha',__('reCaptcha',"tdomf"), 'tdomf_widget_recaptcha_control', 700, 500);
+      }
+     
+     /** 
+     * Display error to user for mis-configuration
+     * 
+     * @access public
+     * @return String
+     */       
+     function adminError($options,$form_id) {
+         $output = "";  
+         if(empty($options['publickey']) || empty($options['privatekey'])) {
+             $output .= __('<b>Error</b>: Widget "reCaptcha" is missing the private and/or public keys and cannot function if theses are not set.','tdomf');
+         }
+         return $output;
+     }      
 
-function tdomf_widget_recaptcha_admin_error($form_id) {
-    
-  $options = tdomf_widget_recaptcha_get_options($form_id,true);
-
-  $output = "";  
-  if(empty($options['publickey']) || empty($options['privatekey'])) {
-      $output .= __('<b>Error</b>: Widget "reCaptcha" is missing the private and/or public keys and cannot function if theses are not set.','tdomf');
   }
   
-  return $output;
-}
-tdomf_register_form_widget_admin_error('recaptcha',__('reCaptcha','tdomf'), 'tdomf_widget_recaptcha_admin_error');
+    
+  // Create and start the widget
+  //
+  global $tdomf_widget_recaptcha;
+  $tdomf_widget_recaptcha = new TDOMF_WidgetReCaptcha();
 
 ?>
